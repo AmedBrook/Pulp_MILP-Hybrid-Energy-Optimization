fabac5a9c238ced9395da53170bb27d0b6435d37
Ahmed <ahmed.mabrouk@novia.fi>
2023-01-23 12:20:13 +0200

    Adding pulp version test

--- test_environment.py
+++ test_environment.py
@@ -1,9 +1,14 @@
 import sys
 
-REQUIRED_PYTHON = "python"
 
+
+############## Testing the python version required #############################
+
+REQUIRED_PYTHON = "python" 
 
 def main():
+    
+    
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
@@ -23,3 +28,25 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+############ Testing the required Pulp packages and version ##################  
+
+import pulp
+
+def main():
+    
+    
+    pulp_version = pulp. VERSION
+        
+    if pulp_version != "2.7.0":
+        raise TypeError(
+            "This project requires Pulp version 2.7.0" )
+    else:
+        print(">>> Pulp version passes test!")
+    
+
+if __name__ == '__main__':
+    main()
+
