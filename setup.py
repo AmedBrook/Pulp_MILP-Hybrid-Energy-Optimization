241ea0d7cdbab55226b3d0d0c8bcb6d5b34488f4
Ahmed <ahmed.mabrouk@novia.fi>
2023-01-25 12:30:31 +0200

    Adding maintainer_eamil

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     name='HYH',
     version='0.1.0',
     authors='Ahmed Mabrouk / Johan Westö / Ray Pörn',
+    maintainer='Ahnmed Mabrouk', 
     description='MILP Hybrid Energy Optimization ',
     url='https://github.com/AmedBrook/Pulp_MILP-Hybrid-Energy-Optimization',
-    author_email='Ahmed.Mabrouk@novia.fi',
+    maintainer_email='Ahmed.Mabrouk@novia.fi',
     license='MIT',
     packages=find_packages(),
     install_requires=[ # those are the requirements for the project packages.
