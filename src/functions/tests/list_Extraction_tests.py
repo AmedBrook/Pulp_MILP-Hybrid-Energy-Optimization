619e652e6c546d961291ce87101bca9660f49381
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-02-07 10:36:05 +0200

    Runnig check and correcting style errors according to PEP8 using flake8

--- src/functions/tests/list_Extraction_tests.py
+++ src/functions/tests/list_Extraction_tests.py
@@ -1,27 +1,42 @@
-# List_Extraction testing routine 
+# testing routine for List_Extraction function
 
 import unittest
+import sys
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+wpath = os.getenv(r'wpath')
+ppath = os.getenv(r'ppath')
+
+sys.path.append(os.path.abspath(os.path.join(ppath)))
+sys.path.append(os.path.abspath(os.path.join(wpath)))
+
 
 from list_extraction import list_extract
 
 
-class list_extract(unittest.TestCase):
+class list_extract_test(unittest.TestCase):
 
     def test_lists_lenght(self):
         """
-        Test that it extrcat the same list length for all target LP varaibles
+        Test if it extracts the same list length for all targeted LP varaibles
         """
         import numpy as np
 
-        dt = 1                                                        # simulation time step dt.
-        t_max = 60                                                    # time span for simulation = t_max hours.
-        t = np.atleast_2d(np.arange(0,t_max,dt)).T.conj()             # time scale in hours.
-        n = len(t)                                                    # number of time steps.
-        V_steps = [x for x in range (0,n)]                            # Time steps vector. 
+        dt = 1    # simulation time step dt.
+        t_max = 60   # time span for simulation = t_max hours.
+        t = np.atleast_2d(np.arange(0, t_max, dt)).T.conj()   # time scale in hours.
+        n = len(t)  # number of time steps.
+        V_steps = [x for x in range(0, n)]  # Time steps vector.
 
-        
-        list_extract('FC_A_', 'P_A_','P_A_load_','P_A_to_bat_','P_From_bat_','Q_bat_','Y_','Y_from_bat_','Y_to_bat_','Z_')
-        
+        list_extract(
+            'FC_A_', 'P_A_', 'P_A_load_',
+            'P_A_to_bat_', 'P_From_bat_',
+            'Q_bat_', 'Y_', 'Y_from_bat_',
+            'Y_to_bat_', 'Z_')
+
         lenn_FC_A_ = len('FC_A_')
         lenn_P_A_ = len('P_A_')
         lenn_P_A_load_ = len('P_A_load_')
@@ -33,16 +48,20 @@ class list_extract(unittest.TestCase):
         lenn_Y_to_bat_ = len('Y_to_bat_')
         lenn_Z_ = len('Z_')
 
-        self.assertEqual(lenn_FC_A_, len(V_steps))
-        self.assertEqual(lenn_P_A_, len(V_steps))
-        self.assertEqual(lenn_P_A_load_, len(V_steps))
-        self.assertEqual(lenn_P_A_to_bat_, len(V_steps))
-        self.assertEqual(lenn_P_From_bat_, len(V_steps))
-        self.assertEqual(lenn_Q_bat_, len(V_steps))
-        self.assertEqual(lenn_Y_, len(V_steps))
-        self.assertEqual(lenn_Y_from_bat_, len(V_steps))
-        self.assertEqual(lenn_Y_to_bat_, len(V_steps))
-        self.assertEqual(lenn_Z_, len(V_steps))
+        # error message in case if test case got failed.
+        message = "the extracted list doesn't fit the load frame"
+
+        self.assertEqual(lenn_FC_A_, len(V_steps), message)
+        self.assertEqual(lenn_P_A_, len(V_steps), message)
+        self.assertEqual(lenn_P_A_load_, len(V_steps), message)
+        self.assertEqual(lenn_P_A_to_bat_, len(V_steps), message)
+        self.assertEqual(lenn_P_From_bat_, len(V_steps), message)
+        self.assertEqual(lenn_Q_bat_, len(V_steps), message)
+        self.assertEqual(lenn_Y_, len(V_steps), message)
+        self.assertEqual(lenn_Y_from_bat_, len(V_steps), message)
+        self.assertEqual(lenn_Y_to_bat_, len(V_steps), message)
+        self.assertEqual(lenn_Z_, len(V_steps), message)
+
 
 if __name__ == '__main__':
     unittest.main()
