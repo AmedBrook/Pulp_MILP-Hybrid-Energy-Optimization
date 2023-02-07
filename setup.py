d2b1bdc8224566f14cd8c4d467f8768f4fbb34f0
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-02-07 15:57:21 +0200

    Adding get_install_requirements() function

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from setuptools import setup, find_packages
 
+def get_install_requirements():    
+
+    """    It reads the requirements.txt file and returns a list of all the requirements    
+           :return: A list of all the requirements for the project.    
+    """    
+    with open("requirements.txt", "r", encoding="utf-8") as f:        
+        reqs = [x.strip() for x in f.read().splitlines()]    
+        reqs = [x for x in reqs if not x.startswith("#")]    
+        return reqs
+    
 setup(
     
     name='HYH',
     version='0.1.0',
-    authors='Ahmed Mabrouk / Johan Westö / Ray Pörn',
-    maintainer='Ahnmed Mabrouk', 
-    description='MILP Hybrid Energy Optimization ',
+    authors='',
+    maintainer='Ahmed Mabrouk', 
+    description='MILP Hybrid Energy Optimization',
     url='https://github.com/AmedBrook/Pulp_MILP-Hybrid-Energy-Optimization',
     maintainer_email='Ahmed.Mabrouk@novia.fi',
     license='MIT',
-    packages=find_packages(),
-    install_requires=[ # those are the requirements for the project packages.
-        'gurobipy==10.0.0'
-        'PuLP==2.7.0'
-        'matplotlib==3.2.2'
-        'numpy==1.21.6' 
+    packages=setuptools.find_packages(include=['HYH']),
+    install_requires=get_install_requirements()
     ]
 )
+
