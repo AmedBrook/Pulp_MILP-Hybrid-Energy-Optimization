5bcd05ff21dbe3a29d2f248fe222f01d67e95f4c
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-02-07 09:33:46 +0200

    Runnig check and correcting style errors according to PEP8 using flake8

--- src/functions/rand_window.py
+++ src/functions/rand_window.py
@@ -1,10 +1,11 @@
 # Generating random Load profil.
-import random
+import random.randrange
+
 
 def Load(r):
-  P_A_max = 1000
-  Profile = {}
-  for i in range(0,59):
-    from random import randrange
-    Profile[i] = randrange (0.2*P_A_max, 0.9*P_A_max)
-  return Profile 
+
+    P_A_max = 1000
+    Profile = {}
+    for i in range(0, 59):
+        Profile[i] = random.randrange(0.2*P_A_max, 0.9*P_A_max)
+    return Profile
