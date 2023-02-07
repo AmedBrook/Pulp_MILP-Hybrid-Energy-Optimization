c08ef3ef8fe7d6776e4f143de92c9a6081a4037e
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-02-07 09:41:03 +0200

    Runnig check and correcting style errors according to PEP8 using flake8

--- src/functions/tests/fuel_consumption_tests.py
+++ src/functions/tests/fuel_consumption_tests.py
@@ -11,10 +11,11 @@ class TestfuelCon(unittest.TestCase):
         """
         Test that it doesn't exceed the maximum fuel consumption allowed.
         """
+        # Normally it should return 163800, 630 is a random power value
         P_A_max = 1000
-        maxcons = fuelCon (0.9*630, P_A_max) # Normally it should return 163800, 630 is a random power value
-        
-        self.assertEqual(maxcons, 163800.0) 
+        maxcons = fuelCon(0.9*630, P_A_max)
+
+        self.assertEqual(maxcons, 163800.0)
 
     def test_neg_value(self):
 
@@ -22,13 +23,14 @@ class TestfuelCon(unittest.TestCase):
         Test that it doesn't return a negative fuel consumption value.
         """
         P_A_max = 1000
-        negcons = fuelCon((x for x in range(0,0.9*P_A_max)), P_A_max)
-        if negcons >= 0: 
+        negcons = fuelCon((x for x in range(0, 0.9*P_A_max)), P_A_max)
+        if negcons >= 0:
             res = 1
         else:
-            res = 0 
+            res = 0
 
         self.assertEqual(res, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
