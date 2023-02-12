b888e62ff0bbe5a24d526c039e5c6d66e3869339
Ahmed <mabrook.amed@gmail.com>
2023-02-12 12:21:59 +0200

    adding P_A_max as Load  argument

--- src/functions/rand_window.py
+++ src/functions/rand_window.py
@@ -1,11 +1,9 @@
 # Generating random Load profil.
-import random.randrange
+import random
 
+def Load(r, P_A_max):
 
-def Load(r):
-
-    P_A_max = 1000
     Profile = {}
-    for i in range(0, 59):
+    for i in range(0, r-1):
         Profile[i] = random.randrange(0.2*P_A_max, 0.9*P_A_max)
     return Profile
