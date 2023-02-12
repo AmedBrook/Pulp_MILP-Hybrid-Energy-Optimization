300d39cab3a703c3828a3aab6f26b8af6e8a41aa
Ahmed <mabrook.amed@gmail.com>
2023-02-12 12:49:41 +0200

    structuring import modules

---
+++ src/functions/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['fuel_consumption', 'list_extraction', 'load_window']
+
+from .fuel_consumption import *
+from .load_window import *
+from .list_extraction import *
+
---
+++ src/tests/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['fuel_consumption_tests', 'load_window_tests', 'list_extraction_tests']
+
+from .fuel_consumption_tests import * 
+from .load_window_tests import * 
+from .list_Extraction_tests import *
