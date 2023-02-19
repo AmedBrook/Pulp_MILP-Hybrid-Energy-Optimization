e70b64f5f284026474771109e906e75e42f50689
Ahmed <mabrook.amed@gmail.com>
2023-02-19 23:13:31 +0200

    Fixing test errors

--- src/tests/fuel_consumption_tests.py
+++ src/tests/fuel_consumption_tests.py
@@ -8,21 +8,21 @@ class TestfuelCon(unittest.TestCase):
 
     def test_max_consumption(self):
         """
-        Test that it doesn't exceed the maximum fuel consumption allowed.
+        Test if it doesn't exceed the maximum fuel consumption allowed.
         """
-        # Normally it should return 163800, 630 is a random power value
+        # Normally it should return 125880.237, 630 is a random power value
         P_A_max = 1000
         maxcons = fuel_consumption.fuelCon(0.9*630, P_A_max)
 
-        self.assertEqual(maxcons, 163800.0)
+        self.assertEqual(maxcons, 125880.237)
 
     def test_neg_value(self):
 
         """
-        Test that it doesn't return a negative fuel consumption value.
+        Test if it doesn't return a negative fuel consumption value.
         """
         P_A_max = 1000
-        negcons = fuel_consumption.fuelCon((x for x in range(0, 0.9*P_A_max)), P_A_max)
+        negcons = fuel_consumption.fuelCon((0.9*P_A_max), P_A_max)
         if negcons >= 0:
             res = 1
         else:
