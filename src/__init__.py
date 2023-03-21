20494ecbfa06ff54c4d0d46b78334b7e1fe2dc56
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-03-21 11:50:36 +0200

    Updating __init__.py file

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,1 @@
 __all__ = ['functions', 'tests']
-
-from .functions import load_window, fuel_consumption, list_extraction
-from .tests import load_window_tests, fuel_consumption_tests, list_Extraction_tests
-
