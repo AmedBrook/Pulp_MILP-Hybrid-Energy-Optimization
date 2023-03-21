443312ab404afc9cbfd8e81b4580862942f4bfa7
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-03-21 11:52:32 +0200

    Deleting empty files

--- src/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['fuel_consumption_tests', 'load_window_tests', 'list_extraction_tests']
-
-from .fuel_consumption_tests import TestfuelCon
-from .load_window_tests import TestLoad
-from .list_Extraction_tests import list_extract_test
