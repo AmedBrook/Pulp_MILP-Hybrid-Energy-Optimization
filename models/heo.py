9ed4f3a2939f9df906b0dadb548051450a0509b8
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-04-03 15:02:45 +0300

    Importing modules

--- models/heo.py
+++ models/heo.py
@@ -4,11 +4,13 @@ Mixed integer energy hybridization problem solved
 with Pulp script to optimize fuel oil consumption
 
 '''
-# Importing libraries.
+# IMPORTING LIBRARIES.
 import numpy as np
 from pulp import *
+from src.functions.load_window import lwd
+from src.functions.fuel_consumption import fuelCon
 
-# Defining set of parameters
+# DEFINING SET OF PARAMETERS.
 Q_max = 250
 Q_0 = 0.5*Q_max
 Q_final = 0.5*Q_max
@@ -25,30 +27,7 @@ fc_offset = 190
 V_steps = [x for x in range(0, n)]
 V_steps_z = V_steps[:-1]
 
-
-# creating load window function.
-def lwd(
- val11, val12, val13,
- val21, val22, val23,
- val31, val32, val33,
- val41, val42, val43,
- L1, L2, L3, L4):
-
-    L = {}
-
-    E1 = np.atleast_2d(np.linspace(val11, val12, val13)).T.conj()
-    E2 = np.atleast_2d(np.linspace(val21, val22, val23)).T.conj()
-    E3 = np.atleast_2d(np.linspace(val31, val32, val33)).T.conj()
-    E4 = np.atleast_2d(np.linspace(val41, val42, val43)).T.conj()
-
-    L1 = dict(enumerate(E1.flatten(), L1))
-    L2 = dict(enumerate(E2.flatten(), L2))
-    L3 = dict(enumerate(E3.flatten(), L3))
-    L4 = dict(enumerate(E4.flatten(), L4))
-    L = {**L1, **L2, **L3, **L4}
-    return L
-
-
+# CONSTRUCTING LOAD PROFILE.
 L = lwd(200, 50, 5,
         50, 300, 5,
         200, 150, 10,
@@ -56,25 +35,17 @@ L = lwd(200, 50, 5,
         0, 5, 20, 30)
 
 
-# creating fuelCon function.
-def fuelCon(P, P_max):
-
-    fc = {}
-    fc = 260 * P - 67 / P_max * P**2
-    return fc
-
-
-# Creating optimization problem variable.
+# CREATING OPTIMIZATION PROBLEM VARIABLE.
 Optim = LpProblem('Energy_Opt', LpMinimize)
 
 
-# calculating the slope and intercept for the problem.
+# CALCULATING THE SLOPE AND INTERCEPT FOR THE PROBLEM.
 a_j = (fuelCon(0.9 * P_max, P_max) - fuelCon(0.2 * P_max, P_max)) / (0.9 * P_max)  # slope.
 b_j = fuelCon(0.2*P_max, P_max) - a_j*0.2*P_max  # Intercept.
 maxFC = fuelCon(0.9*P_max, P_max)  # Max fuel bound.
 
 
-# setting up the LP variables.
+# SETTING UP THE LP VARIABLES.
 Q_bat = pulp.LpVariable.dicts("Q_bat", V_steps, lowBound=0.2 * Q_max, upBound=Q_max, cat=LpContinuous)
 P_From_bat = pulp.LpVariable.dicts("P_From_bat", V_steps, lowBound=P_min, upBound=0.9 * P_max, cat=LpContinuous)
 P = pulp.LpVariable.dicts("P", V_steps, lowBound=P_min, upBound=0.9 * P_max, cat=LpContinuous)
@@ -94,7 +65,7 @@ Optim += lpSum(FC + L_added_cost), "Minimization fuel oil consumption objective"
 print(FC)
 
 
-# Setting up problem constraints.
+# SETTING UP THE OBJECTIF FUNCTION.
 for k in V_steps:
 
     # Fuel oil consumption constraint.
@@ -120,22 +91,22 @@ for k in V_steps:
     else:
         Optim += Q_bat[k] == Q_bat[k-1] + eff_to_bat*P_to_bat[k]*dt - P_From_bat[k]*dt
 
-# Additional starting costs constraint.
+# ADDITIONAL STARTING COSTS CONSTRAINT.
 for k in range(V_steps[0], V_steps[-1]):
     Optim += Z[k] >= Y[k + 1] - Y[k]
 
-# Charge balance at the Final time step.
+# CHARGE BALANCE AT THE FINAL TIME STEP.
 Optim += Q_bat[V_steps[-1]] == Q_final
 
 
-# Solving the prblem using Gurobi solver.
+# SOLVING THE PRBLEM USING GUROBI SOLVER.
 status = Optim.solve(GUROBI())
 
 
-# Printing the LP optimal values.
+# PRINTING THE LP OPTIMAL VALUES.
 for v in Optim.variables():
     print(v.name, "=", v.varValue)
 
 
-# Printing total ptimized fuel oil consuption.
+# PRINTING TOTAL PTIMIZED FUEL OIL CONSUPTION.
 print("Total fuel comsumption of the trip:", value(Optim.objective), 'g')
