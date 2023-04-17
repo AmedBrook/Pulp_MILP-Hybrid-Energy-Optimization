b882d584d55618dd9cc48a9c4ec178186d07d188
Ahmed Mabrouk <mabrook.amed@gmail.com>
2023-04-17 11:50:29 +0300

    Updating list_extraction.py

--- src/functions/list_extraction.py
+++ src/functions/list_extraction.py
@@ -5,25 +5,24 @@
 def lixtr():
     import numpy as np
     import pulp
-    pulp.pulpTestAll()
 
     dt = 1
-    t_max = 60
+    t_max = 10
     t = np.atleast_2d(np.arange(0, t_max, dt)).T.conj()
     n = len(t)
     V_steps = [x for x in range(0, n)]
     Optim = pulp.LpProblem('Energy_Opt', pulp.LpMinimize)
 
-    FC_ = ([] for i in range(10))
-    P_ = ([] for i in range(10))
-    P_load_ = ([] for i in range(10))
-    P_to_bat_ = ([] for i in range(10))
-    P_From_bat_ = ([] for i in range(10))
-    Q_bat_ = ([] for i in range(10))
-    Y_ = ([] for i in range(10))
-    Y_from_bat_ = ([] for i in range(10))
-    Y_to_bat_ = ([] for i in range(10))
-    Z_ = ([] for i in range(10))
+    FC_ = ([] for i in range(len(V_steps)))
+    P_ = ([] for i in range(len(V_steps)))
+    P_load_ = ([] for i in range(len(V_steps)))
+    P_to_bat_ = ([] for i in range(len(V_steps)))
+    P_From_bat_ = ([] for i in range(len(V_steps)))
+    Q_bat_ = ([] for i in range(len(V_steps)))
+    Y_ = ([] for i in range(len(V_steps)))
+    Y_from_bat_ = ([] for i in range(len(V_steps)))
+    Y_to_bat_ = ([] for i in range(len(V_steps)))
+    Z_ = ([] for i in range(len(V_steps)))
 
     nms = {
      0: FC_,
@@ -61,3 +60,6 @@ def lixtr():
         print((nmss[nm]) + '=', (nms[nm]))
 
     return lixtr
+
+
+lixtr()
